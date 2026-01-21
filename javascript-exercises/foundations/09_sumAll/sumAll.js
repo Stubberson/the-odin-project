f8d04ad3819f8c6796db581005f184565227b8e3
Stubberson <tuukka.j.korpela@gmail.com>
2026-01-21 16:35:41 +0100

    Create a function that sums a range

--- javascript-exercises/foundations/09_sumAll/sumAll.js
+++ javascript-exercises/foundations/09_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    // Only accept positive integers
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) return 'ERROR'
 
+    let start = Math.min(a, b)
+    let end = Math.max(a, b)
+    // The {length: x} is an array-like object with the desired length
+    let array = Array.from({length: end - start + 1}, (_, index) => start + index)
+    return array.reduce((acc, current) => acc + current)
 };
 
 // Do not edit below this line
