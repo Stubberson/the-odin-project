5b92a712cf63aba749aa94794686f642cb6ed14a
Stubberson <tuukka.j.korpela@gmail.com>
2026-01-21 15:18:44 +0100

    Write a remove from array -function

--- javascript-exercises/foundations/08_removeFromArray/removeFromArray.js
+++ javascript-exercises/foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...items) {
+    return array.filter(item => items.includes(item) === false)
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
