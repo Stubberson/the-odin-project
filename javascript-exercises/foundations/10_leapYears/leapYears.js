5b9d25e149b1606e419e7c30cb2b379d534a4b31
Stubberson <tuukka.j.korpela@gmail.com>
2026-01-21 16:50:43 +0100

    Leap year exercise

--- javascript-exercises/foundations/10_leapYears/leapYears.js
+++ javascript-exercises/foundations/10_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+// A leap year is a year which is both evenly divisible by 4 and not divisible by 100
+// or is evenly divisible by 400.
+const leapYears = function(year) {
+    if ((year % 4 === 0 && year % 100 != 0) || year % 400 === 0) return true
+    else return false
 };
 
 // Do not edit below this line
