98bbc6689ff3d2564f0a36b5347780c78e9cb501
Stubberson <tuukka.j.korpela@gmail.com>
2026-02-04 19:16:03 +0100

    Remove javascript folder, it's its own repo

--- javascript-exercises/foundations/08_removeFromArray/removeFromArray.js
+++
@@ -1,6 +0,0 @@
-const removeFromArray = function(array, ...items) {
-    return array.filter(item => items.includes(item) === false)
-}
-
-// Do not edit below this line
-module.exports = removeFromArray;
--- javascript-exercises/foundations/10_leapYears/leapYears.js
+++
@@ -1,9 +0,0 @@
-// A leap year is a year which is both evenly divisible by 4 and not divisible by 100
-// or is evenly divisible by 400.
-const leapYears = function(year) {
-    if ((year % 4 === 0 && year % 100 != 0) || year % 400 === 0) return true
-    else return false
-};
-
-// Do not edit below this line
-module.exports = leapYears;
